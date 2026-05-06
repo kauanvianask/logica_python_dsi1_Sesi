6e11f6a033b86eb56b22d7b9f26c55e6f37e0f26
Kauan Viana <kauanvianasoares10@gmail.com>
2026-05-06 11:50:44 -0300

    adicionado o exemplo 2 e 3 no código

--- 1.1-numeros-em-python.py
+++ 1.1-numeros-em-python.py
@@ -30,3 +30,38 @@ print ("Valor:" , numero_inteiro)
 print ("Tipo:", type(numero_inteiro))
 
 print ("--------------------------------------------------------")
+
+#  EXEMPLO 02 - NUMERO DECIMAL
+
+# Criamos uma variável
+numero_decimal = 3.14
+
+# mostramos o valor
+print ("Valor:" , numero_decimal)
+
+# Type() tipo da variável 
+print ("Tipo:" , type(numero_decimal))
+
+print ("--------------------------------------------------------")
+
+# EXEMPLO 03 - NUMERO COMPLEXO
+
+# Possui duas partes
+# Parte real (Numero normal)
+# Parte imaginária (multiplicada por j)
+
+# Estrutura Geral:
+# numero = a + bj 
+
+# a = parte real
+# b = parte imaginária
+# j = unidade imaginária
+
+numero_complexo = 2 + 3j 
+
+print ("Valor:" , numero_complexo)
+
+# Type() tipo da variável
+print ("Tipo:" , type(numero_complexo))
+
+print ("--------------------------------------------------------")
