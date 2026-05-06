530547afafe0b892ce4aec9a35f93c86d963fa0e
Kauan Viana <kauanvianasoares10@gmail.com>
2026-05-06 11:25:01 -0300

    primeiro programa do senai em python

---
+++ 1.1-numeros-em-python.py
@@ -0,0 +1,32 @@
+# AULA COMPLETA: NUMEROS EM PYTHON 
+"""""
+vamos aprender: 
+1 - tipos numericos
+2 - conversões de tipos
+3 - hierarquia numérica
+4 - opercaões matemáticas
+5 - coerção de tipos 
+6 - verificação de tipos 
+7- entrada de dados
+"""
+# ===========================
+# PASSO 01 - TIPOS NUMÉRICOS 
+# ===========================
+# int -> interios 
+# float -> com casas decimais 
+# complex -> complexos (usado em matemática/engenharia)
+
+print ("===== TIPOS NUMÉRICOS =====")
+
+# EXEMPLOS 01 - NUMERO INTEIRO 
+
+# criamos uma variável chamada numero_inteiro
+numero_inteiro = 10 
+
+# mostramos o valor
+print ("Valor:" , numero_inteiro)
+
+# Type() mostra qual é o tipo da variável
+print ("Tipo:", type(numero_inteiro))
+
+print ("--------------------------------------------------------")
