9d6eaa4fe83d6dec01eb8047e1fc4bb4fad7602f
Kauan Viana <kauanvianasoares10@gmail.com>
2026-05-13 16:20:40 -0300

    Atividade

---
+++ atividade-python-1.py
@@ -0,0 +1,81 @@
+# EX1
+# Um aluno tem 10 anos. Armazene essa idade em uma variável
+# e exiba seu tipo.
+idade = int(10)
+print ("idade do menino é", idade)
+print ("tipo: ", type(idade))
+
+print ("----------------------------------------")
+
+# EX2
+# A temperatura medida é 23.5°C.
+# Armazene esse valor e mostre seu tipo.
+temperatura = float(23.5)
+print("Temperatura média é de: ", temperatura)
+print ("tipo: ", type(temperatura))
+
+print ("----------------------------------------")
+# EX3
+# Crie um número complexo representando uma impedância elétrica
+# de 5 + 8j e mostre sua parte real.
+numero_complexo = 5 + 8j
+print ("Valor: " , numero_complexo)
+print ("tipo: " , type(numero_complexo))
+print("Parte Real: ", numero_complexo.real)
+
+print ("----------------------------------------")
+# EX4
+# Mostre a parte imaginária do número complexo
+# criado no exercício anterior.
+print("Parte imaginária:", numero_complexo.imag)
+
+
+print ("----------------------------------------")
+# EX5
+# Declare uma variável chamada "populacao"
+# com o valor 8_000_000_000 (8 bilhões)
+# e mostre seu tipo.
+
+print ("----------------------------------------")
+populacao = int(8_000_000_000)
+print("população é do" )
+print ("tipo: " , type(populacao))
+
+
+print ("----------------------------------------")
+# EX6
+# Verifique se o número 7 é do tipo int
+# usando a função type().
+
+valor2 = 7
+print ("número 7 é do")
+print ("tipo: " , type(valor2))
+
+print ("----------------------------------------")
+# EX7
+# Crie uma variável chamada "aprovado"
+# com o valor booleano True e mostre seu tipo.
+aprovado = True
+print ("Valor booleano aprovado")
+print("tipo: ", type(aprovado))
+
+print ("----------------------------------------")
+# EX8
+# Some True e False e mostre o resultado
+# e também o tipo do resultado.
+
+aprovado = True + False
+print ("Valor: " , aprovado)
+print("tipo: ", type(aprovado))
+
+print ("----------------------------------------")
+# EX9
+# Pesquise e mostre qual é o valor máximo
+# que um número inteiro pode ter em Python.
+print ("Não existe um número máximo com base nas minhas pesquisas")
+
+print ("----------------------------------------")
+# EX10
+# Mostre a representação em binário
+# do número 10 usando uma função do Python.
+print ("representação: " , bin(10))
